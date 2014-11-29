e78a9213d0df1a065ef6347e074024210d1b8e65
zhfuzzy <zhfuzzy@gmail.com>
2014-11-29 22:41:09 +0800

    Adding mode-line indicator of IME state

--- evil-fcitx.el
+++ evil-fcitx.el
@@ -4,9 +4,23 @@
 ; `$ fcitx-remote -c' will set IME to 1 - default IME
 ; `$ fcitx-remote -o' will set IME to 2 - user's IME
 
-; ime-user-state
+;;;;;;;;;; User configs
+
+; for me I don't use C-q quite often
 (defvar back-to-default-state-key "C-q")
+(defalias 'q 'quote)
+
+; Indicator to be displayed in mode-line, like <N>/<I> for Evil-mode
+(defvar user-IME-state-indicator "[ZH]")
+(defvar default-IME-state-indicator "[EN]")
+
+; 2 is the return value of `$ fcitx-remote' when you toggle on your IME
+; usaully don't need to change this
 (defvar user-IME-ID "2\n")
+
+;;;;;;;;;; End of user configs
+
+(defvar IME-state-mode-line-tag default-IME-state-indicator)
 
 (defun get-current-IME ()
   (if (equal (shell-command-to-string "fcitx-remote")
@@ -24,21 +38,35 @@
 (defun switch-to-user-IME ()
   (shell-command "fcitx-remote -o"))
 
+(defun IME-state-setter (state)
+  (setq IME-state state)
+  (cond
+   ((string-equal state "default-IME-state")
+	(setq IME-state-mode-line-tag "[EN]"))
+   ((string-equal state "user-IME-state")
+	(setq IME-state-mode-line-tag "[ZH]")))
+  (force-mode-line-update))
+
 (defun exit-insert-evil-fcitx ()
   "When exit insert with user-IME, set IME-state to user-IME-state"
   (if (equal (get-current-IME) "user-IME")
 	  (progn
-		(setq IME-state "user-IME-state")
+		(IME-state-setter "user-IME-state")
 		(switch-to-default-IME)
 		; print-msg
 		(shell-command "echo \"IME state recorded\""))
-	(setq IME-state "default-IME-state")))
+	(IME-state-setter "default-IME-state")))
 
 (add-hook 'evil-insert-state-exit-hook
 		  'exit-insert-evil-fcitx)
 
 (defun entry-insert-evil-fcitx ()
   "When entry insert with user-IME-state, switch back to user-IME"
+  ; clear this tag in insert mode
+  (progn 
+	(setq IME-state-mode-line-tag "")
+	(force-mode-line-update))
+  (print "test point")
   (if (and (equal IME-state "user-IME-state"))
 	  (progn
 		(switch-to-user-IME)
@@ -50,11 +78,11 @@
 
 (defun back-to-default-state()
   (interactive)
-  (setq IME-state (get-current-IME))
-  (if (equal IME-state "user-IME")
+  (if (or (string-equal IME-state "user-IME-state")
+		  (string-equal (get-current-IME) "user-IME"))
 	  (progn
 		(switch-to-default-IME)
-		(setq IME-state "default-IME-state")
+		(IME-state-setter "default-IME-state")
 		(evil-normal-state)
 		(shell-command "echo \"IME-state cleanred\""))
 	(shell-command "echo \"unchanged\"")))
